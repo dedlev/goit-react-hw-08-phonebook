cde5729fbb7d9abc9b7b93070bd6730a63048534
dedlev <l.popchuk@gmail.com>
2025-01-11 21:13:46 +0200

    delete index.css

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,25 +1,29 @@
 import React from 'react';
 import { Box, Typography } from '@mui/material';
 import ContactPageIcon from '@mui/icons-material/ContactPage';
+import { GlobalStyle } from "styles/GlobalStyle";
 
 const MyComponent = () => {
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        justifyContent: 'center',
-        backgroundColor: '#F5F5F5',
-        alignItems: 'center',
-        height: '100vh',
-        textAlign: 'center',
-        color: '#1976d2',
-      }}
-    >
-      <ContactPageIcon sx={{ fontSize: '3rem', marginRight: '0.5rem' }} />
-      <Typography variant="h1" component="h1">
-        Contacts manager welcome page
-      </Typography>
-    </Box>
+    <div>
+      <Box
+    sx={{
+      display: 'flex',
+      justifyContent: 'center',
+      backgroundColor: '#F5F5F5',
+      alignItems: 'center',
+      height: '100vh',
+      textAlign: 'center',
+      color: '#1976d2',
+    }}
+  >
+    <ContactPageIcon sx={{ fontSize: '3rem', marginRight: '0.5rem' }} />
+    <Typography variant="h1" component="h1">
+      Contacts manager welcome page
+    </Typography>
+  </Box>
+  <GlobalStyle />
+  </div>
   );
 };
 
